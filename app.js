d46c5734fa56f5d054fbf582fe305d8a200e88c2
vineetjm <vineetmariguddi09@gmail.com>
2024-01-04 00:15:05 +0530

    after adding autherization

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
+
 const methodOverride = require("method-override");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const MongoUrl = "mongodb://127.0.0.1:27017/wonderlust";
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./Schema.js");
-const Review = require("./models/review.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -39,9 +41,17 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+// to add authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  res.locals.currUser = req.user; // Used in sending user info for navbar
   next();
 });
 
@@ -57,8 +67,12 @@ async function main() {
   await mongoose.connect(MongoUrl);
 }
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+console.log("owner: vineet=>abc");
+console.log("user1=>abc");
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not Found !"));
